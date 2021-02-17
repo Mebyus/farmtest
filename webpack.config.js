7bf8f6e19f108d0a6588d2996ac02deb98a2e48a
Mebyus <mebyus@gmail.com>
2021-02-17 12:10:36 +0400

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/entry/index.ts',
+    target: 'web',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                options: { emitErrors: true },
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'entry', 'index.html'),
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+};
